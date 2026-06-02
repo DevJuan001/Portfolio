a2edd7cec05109a806c7901ba4fa3790ea367e65
DevJuan001 <juanesyt7@gmail.com>
2026-06-02 18:29:16 -0500

    feat: integrate theme hook and add SectionsContainer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useTheme } from "./hooks/useTheme";
 import Header from "./components/Header";
+import SectionsContainer from "./components/SectionsContainer";
 
 function App() {
+  useTheme();
+
   return (
-    <div>
+    <div className="w-full h-full">
       <Header />
+      <SectionsContainer />
+      <div id="modal-root"></div>
     </div>
   );
 }
