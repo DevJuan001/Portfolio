3bcfb9201a1736b9415e236aa869448b3aa39904
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 16:01:10 -0500

    style(ProjectModal): adjust button positioning and enhance shadow effects for improved UI aesthetics

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -25,15 +25,15 @@ export default function ProjectModal({ project }) {
           </div>
         ))}
 
-        <div className="absolute bottom-3 left-0 right-0 flex items-center justify-center">
+        <div className="absolute bottom-4 left-0 right-0 flex items-center justify-center">
           <div
-            className="h-10 flex gap-2 px-3 py-4 rounded-3xl backdrop-blur-md animate-carrousel-sections-in
-            dark:backdrop-blur-sm"
+            className="h-10 flex gap-2 px-3 py-4 rounded-3xl backdrop-blur-md shadow-[0px_0px_1px_0.01px_#28282b8c] animate-carrousel-sections-in
+            dark:backdrop-blur-sm dark:shadow-[0px_0px_5px_0.01px_#28282b8c]"
           >
             {images.map((_, index) => (
               <button
                 onClick={() => goTo(index)}
-                className={`h-2 rounded-3xl transition-all duration-500
+                className={`h-2 rounded-3xl transition-all duration-500 shadow-[0px_0px_6px_0.01px_#28282b8c]
                 ${index === current ? "w-8 bg-white dark:bg-[#28282b]" : "w-2 bg-[#ffffff5c] dark:bg-[#28282b8c]"}
                 hover:cursor-pointer
               `}
