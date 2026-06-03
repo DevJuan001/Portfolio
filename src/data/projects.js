665db7e1b2680fbf484692d545093ad73c1086be
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 00:13:01 -0500

    feat(projects): update Tracklinker project details including link, GitHub repository, and technology stack

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,10 +2,10 @@ export const projects = [
   {
     title: "Tracklinker",
     description: "afsfsfsfsfsfsfsfsfsfsfsfsfs",
-    link: "https://svgl.vercel.app/",
-    github: "https://github.com/pheralb/svgl",
+    link: "https://tracklinker-frontend-web.vercel.app/",
+    github: "https://github.com/DevJuan001/Tracklinker-frontend-web",
     image: "/projects/tracklinker.webp",
     alt: "Proyecto tracklinker",
-    tags: ["NEXT", "TAILWIND"],
+    stack: ["Python", "FastAPI", "Redis", "React", "Tailwind", "Tanstack"],
   },
 ];
