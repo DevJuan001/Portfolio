e2be74b1f536c43bcba3faef9dfd0a6e17a90a5e
DevJuan001 <juanesyt7@gmail.com>
2026-06-02 23:40:17 -0500

    feat(icons): import and register new SVG icons for FastAPI, GSAP, MySQL, Python, React, Redis, Tailwind CSS, and TanStack

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,4 +1,12 @@
+import Gsap from "./icons/gsap.svg?react";
+import MySql from "./icons/mysql.svg?react";
+import Redis from "./icons/redis.svg?react";
+import Python from "./icons/python.svg?react";
+import Fastapi from "./icons/fastApi.svg?react";
+import ReactIcon from "./icons/react.svg?react";
+import Tanstack from "./icons/tanstack.svg?react";
 import Linkedin from "./icons/linkedin.svg?react";
+import Tailwind from "./icons/tailwindcss.svg?react";
 import GithubDark from "./icons/GitHub_dark.svg?react";
 import GithubLight from "./icons/GitHub_light.svg?react";
 
@@ -6,4 +14,12 @@ export const icons = {
   linkedin: Linkedin,
   githubDark: GithubDark,
   githubLight: GithubLight,
+  fastapi: Fastapi,
+  gsap: Gsap,
+  mysql: MySql,
+  python: Python,
+  react: ReactIcon,
+  redis: Redis,
+  tailwind: Tailwind,
+  tanstack: Tanstack,
 };
