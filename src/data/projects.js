75345a28be5438631ebe258680735d44563c18ed
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 15:49:41 -0500

    feat(Tracklinker): update project descriptions and enhance image handling for Tracklinker project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,15 @@
 export const projects = [
   {
     title: "Tracklinker",
-    description: "afsfsfsfsfsfsfsfsfsfsfsfsfs",
+    short_description: `Tracklinker es una aplicación web de gestión de inventarios que permite administrar productos, categorías, proveedores y órdenes de entrada/salida.
+    Incluye autenticación con control de acceso por roles, visualización de estadísticas mediante gráficos interactivos y exportación de reportes en PDF y Excel.`,
+    description: `Tracklinker es un sistema web de gestión de inventarios diseñado para optimizar el control de stock de pequeñas y medianas empresas.
+    La plataforma permite organizar el inventario mediante categorías y subcategorías, registrar productos con su información detallada, gestionar proveedores y procesar órdenes de entrada y salida de mercancía.
+    Cuenta con un módulo de estadísticas con gráficos interactivos que facilitan la toma de decisiones basada en datos, y un sistema de exportación de reportes en formato PDF y Excel.
+    La autenticación es segura y el sistema adapta su interfaz dinámicamente según el rol del usuario, mostrando únicamente las funcionalidades y vistas correspondientes a cada perfil de acceso.`,
     link: "https://tracklinker-frontend-web.vercel.app/",
     github: "https://github.com/DevJuan001/Tracklinker-frontend-web",
-    image: "/projects/tracklinker.webp",
+    images: ["/projects/tracklinker-1.webp", "/projects/tracklinker-2.png", "/projects/tracklinker-2.png"],
     alt: "Proyecto tracklinker",
     stack: ["Python", "FastAPI", "Redis", "React", "Tailwind", "Tanstack"],
   },
