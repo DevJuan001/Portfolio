c89cd0f036262505cdf8ea608ef9dbaeeebe3300
DevJuan001 <juanesyt7@gmail.com>
2026-06-02 22:32:50 -0500

    feat: add socials.js to define social media links with icons for GitHub and LinkedIn

---
+++ src/data/socials.js
@@ -0,0 +1,15 @@
+import { icons } from "../assets/icons";
+
+export const socials = [
+  {
+    title: "Github",
+    href: "https://github.com/DevJuan001",
+    icon: icons.githubLight,
+    iconDarkStyles: "dark:invert"
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/juan-alvarez-953088316/",
+    icon: icons.linkedin,
+  },
+];
