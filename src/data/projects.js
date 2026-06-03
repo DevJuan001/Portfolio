5cf1a91c4f52e3d0cccf58a598b650a34308e8fe
DevJuan001 <juanesyt7@gmail.com>
2026-06-02 23:05:36 -0500

    feat(projects): add initial projects data for Tracklinker

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {
+    title: "Tracklinker",
+    description: "afsfsfsfsfsfsfsfsfsfsfsfsfs",
+    link: "https://svgl.vercel.app/",
+    github: "https://github.com/pheralb/svgl",
+    image: "/projects/tracklinker.webp",
+    alt: "Proyecto tracklinker",
+    tags: ["NEXT", "TAILWIND"],
+  },
+];
