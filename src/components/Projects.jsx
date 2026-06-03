31426643a00663f4098f3ed5af5d98341c11313b
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 00:13:09 -0500

    feat(projects): extend project details to include technology stack mapping

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,19 @@
 import { projects } from "../data/projects";
+import { technologies } from "../data/technologies";
 import ProjectItem from "./ProjectItem";
 
 export default function Projects() {
+  const extendedProjects = projects.map((project) => ({
+    ...project,
+    stack: project.stack.map((technology) => technologies[technology]),
+  }));
+
   return (
     <section id="projects" className="mx-auto mt-20 mb-32 lg:max-w-6xl">
       <h2 className="text-3xl font-bold dark:text-white">Proyectos</h2>
 
       <div className="mt-10 grid grid-cols-1 lg:grid-cols-2">
-        {projects.map((project) => (
+        {extendedProjects.map((project) => (
           <ProjectItem
             key={project.title}
             image={project.image}
@@ -16,6 +22,7 @@ export default function Projects() {
             description={project.description}
             link={project.link}
             github={project.github}
+            stack={project.stack}
           />
         ))}
       </div>
