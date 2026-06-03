c92aa74b16ea186ea5fcec51c46448b76b32c911
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 00:17:15 -0500

    feat(technologies): add technology data structure with associated styles and icons

---
+++ src/data/technologies.js
@@ -0,0 +1,39 @@
+import { icons } from "../assets/icons";
+
+export const technologies = {
+  Python: {
+    name: "Python",
+    styles: "bg-blue-950 text-white",
+    icon: icons.python,
+  },
+
+  FastAPI: {
+    name: "FastAPI",
+    styles: "bg-black text-white",
+    icon: icons.fastapi,
+  },
+
+  Redis: {
+    name: "Redis",
+    styles: "bg-red-700 text-white",
+    icon: icons.redis,
+  },
+
+  React: {
+    name: "React",
+    styles: "bg-blue-950 text-white",
+    icon: icons.react,
+  },
+
+  Tailwind: {
+    name: "Tailwind",
+    styles: "bg-blue-950 text-white",
+    icon: icons.tailwind,
+  },
+
+  Tanstack: {
+    name: "Tanstack",
+    styles: "bg-black text-white",
+    icon: icons.tanstack,
+  },
+};
