808c126062a197d2a7333208c0961d1322f015f2
DevJuan001 <juanesyt7@gmail.com>
2026-06-03 16:18:05 -0500

    style(SectionsContainer): reduce bottom padding for improved layout consistency

--- src/components/SectionsContainer.jsx
+++ src/components/SectionsContainer.jsx
@@ -5,7 +5,7 @@ import Projects from "./Projects";
 
 export default function SectionsContainer() {
   return (
-    <main className="px-8 pb-16">
+    <main className="px-8 pb-8">
       <Hero />
       <Projects />
       <Contact />
