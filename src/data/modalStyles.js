91f2fa5fadc1aa62bcb41ba30a2d8397968897b0
DevJuan001 <juanesyt7@gmail.com>
2026-06-20 18:07:58 -0500

    style(modalStyles): update project modal width for improved responsiveness on larger screens

--- src/data/modalStyles.js
+++ src/data/modalStyles.js
@@ -1,6 +1,6 @@
 export const modal_styles = {
   theme: "p-2 w-[200px] rounded-[32px]",
-  project: "p-6 w-screen rounded-none md:w-3xl md:rounded-[32px] lg:w-4xl",
+  project: "p-6 w-screen rounded-none md:w-6xl md:rounded-[32px]",
   default: "p-7 w-[400px] md:w-[500px] rounded-[32px]",
 };
 
