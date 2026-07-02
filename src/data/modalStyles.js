d12f6cc9d3454f54647cd4ab1437d1efbb9327d9
DevJuan001 <juanesyt7@gmail.com>
2026-07-02 17:47:21 -0500

    style(modalStyles): simplify project modal width for improved layout consistency

--- src/data/modalStyles.js
+++ src/data/modalStyles.js
@@ -1,6 +1,5 @@
 export const modal_styles = {
   theme: "p-2 w-[200px] rounded-[32px]",
-  project: "p-6 w-screen rounded-none md:w-6xl md:rounded-[32px]",
+  project: "p-6 w-screen rounded-none",
   default: "p-7 w-[400px] md:w-[500px] rounded-[32px]",
 };
-
