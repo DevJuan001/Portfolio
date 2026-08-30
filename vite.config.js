ee973019153363bf5b4fb7a028a4f27dab7d84a8
DevJuan001 <juanesyt7@gmail.com>
2026-08-29 20:23:18 -0500

    feat(vite.config): add path aliases for improved module resolution and organization

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@modals": path.resolve(__dirname, "./src/components/modals"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
 });
