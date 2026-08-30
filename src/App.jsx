37cbfcb168ce9a26b6430c4b8d8088e331c7689e
DevJuan001 <juanesyt7@gmail.com>
2026-08-30 17:43:42 -0500

    refactor(App): update Header import to Navbar and adjust layout for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useTheme } from "@hooks/useTheme";
-import Header from "@components/ui/Header";
+import Header from "@/components/ui/Navbar";
 import SectionsContainer from "@components/ui/SectionsContainer";
 
 function App() {
   useTheme();
 
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col">
       <Header />
 
       <SectionsContainer />
